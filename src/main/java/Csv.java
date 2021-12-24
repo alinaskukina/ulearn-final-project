bdcf9ed16c4142088200ea8d89905bb566fc597e
alinaskukina <alinaskukina@yandex.ru>
2021-12-24 22:16:08 +0500

    Created Csv class for .csv parsing

---
+++ src/main/java/Csv.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Csv {
+    public static List<Product> ParseCsv(String filePath) throws IOException {
+        List<Product> products = new ArrayList<>();
+        List<String> fileLines = Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
+        fileLines.remove(0);
+
+        for (String fileLine : fileLines) {
+            String[] splitedText = fileLine.split(";");
+            ArrayList<String> columnList = new ArrayList<>();
+
+            for (String s : splitedText) {
+                //Если колонка начинается на кавычки или заканчивается на кавычки
+                if (IsColumnPart(s)) {
+                    String lastText = columnList.get(columnList.size() - 1);
+                    columnList.set(columnList.size() - 1, lastText + "," + s);
+                } else {
+                    columnList.add(s);
+                }
+            }
+            Product product = new Product();
+            product.Name = columnList.get(0);
+            product.Id = columnList.get(1);
+            product.Price = Double.parseDouble(columnList.get(2));
+            product.Amount = Integer.parseInt(columnList.get(3));
+            products.add(product);
+        }
+        return products;
+    }
+
+    private static boolean IsColumnPart(String text) {
+        String trimText = text.trim();
+        //Если в тексте одна кавычка и текст на нее заканчивается значит это часть предыдущей колонки
+        return trimText.indexOf("\"") == trimText.lastIndexOf("\"") && trimText.endsWith("\"");
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         var connection = Db.getConnection("jdbc:sqlite:./catalog.sqlite");
         try {
+            var data = Csv.ParseCsv("./catalog.csv");
             if (connection != null) {
                 connection.close();
             }
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
     }
